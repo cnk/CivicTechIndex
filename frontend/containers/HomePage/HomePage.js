7fb003faab09d56515f2075b07ba15b7f1a7909e
Cynthia Kiser <cynthiakiser@gmail.com>
2020-11-29 19:38:23 -0800

    Remove Header and Footer from pages until we refactor to get rid of 'You should not use <Link> outside a <Router>'

--- frontend/containers/HomePage/HomePage.js
+++ frontend/containers/HomePage/HomePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { basePageWrap } from '../BasePage';
-import Header from '../../components/Header';
+import Hero from '../../components/Hero';
 import s from './HomePage.module.css';
 
 const HomePage = ({ title }) => {
     return (
         <div className={s.Container}>
-            <Header title={title} />
+            <Hero title={title} />
         </div>
     );
 };
